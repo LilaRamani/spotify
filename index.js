bec0f22bc4aabe62a38daa4f96294f47ace7cca2
Caroline Ramani <LRamani12@gmail.com>
2015-12-07 17:03:10 -0500

    hey

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ $(document).ready( function() {
 	console.log("hi");
 	$.ajax({
             type:"POST",
-            headers:
-            {
-                'Authorization': 'Basic YTdkY2YxYTZkNDljNDdkMGI0ZGQyNjRkN2RhODgxMDk6IDcxZjlkNTdjNzYwNTQ0Y2VhMzc2MWQxM2E1YmFkZmM4DQo=',
-                'Content-Type': 'application/json'
+            headers: {
+                'Authorization':'Basic YTdkY2YxYTZkNDljNDdkMGI0ZGQyNjRkN2RhODgxMDk6IDcxZjlkNTdjNzYwNTQ0Y2VhMzc2MWQxM2E1YmFkZmM4DQo=',
+                'Content-Type':'application/json'
             },
             url: "https://accounts.spotify.com/api/token",
             dataType: 'json',
-            data: "grant_type=client_credentials",
-            processData: false,
+            data: {grant_type: 'client_credentials'}
             success: function(msg) {
             	 console.log(data);
             }
     });
 });
+
